a56e0dc89fa030c3c2dd0dd41ce81b415f724eb8
Marcelo Colonia <mcoloniab18@gmail.com>
2025-03-13 03:43:46 -0500

    added vs2019 gen files

---
+++ generate.bat
@@ -0,0 +1,1 @@
+call vendor\premake5\bin\premake5.exe vs2019
---
+++ premake5.lua
@@ -0,0 +1,39 @@
+workspace "Cipher"
+    architecture "x64"
+    startproject "Cipher"
+    configurations
+    {
+        "Debug",
+        "Release"
+    }
+    flags
+    {
+        "MultiProcessorCompile"
+    }
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Cipher"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++20"
+    staticruntime "on"
+    targetdir ("../bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("../bin-int/" .. outputdir .. "/%{prj.name}")
+    files
+    {
+        "src/**.h",
+        "src/**.cpp"
+    }
+    includedirs
+    {
+        "src",
+    }
+    defines "_CRT_SECURE_NO_WARNINGS"
+    filter "system:windows"
+        systemversion "latest"
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
